f60ad89367e388b6f58646de16e1df5c60064c6c
resteve <resteve@zikzakmedia.com>
2015-12-01 11:12:38 +0100

    Tests

--- tests/test_product_barcode.py
+++ tests/test_product_barcode.py
@@ -1,25 +1,14 @@
-#!/usr/bin/env python
-# This file is part product_barcode module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
+# This file is part of the product_barcode module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class ProductBarcodeTestCase(unittest.TestCase):
+class ProductBarcodeTestCase(ModuleTestCase):
     'Test Product Barcode module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('product_barcode')
-
-    def test0005views(self):
-        'Test views'
-        test_view('product_barcode')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'product_barcode'
 
 
 def suite():
